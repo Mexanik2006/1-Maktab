f2bb89c60aa5b6b868d69cfe1ea3df88099ec39d
mexanik2006 <mexhanik2006@gmail.com>
2023-07-05 18:38:47 +0500

    sss

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -2,7 +2,10 @@ import React from 'react';
 import "./Footer.css";
 import { ImFacebook, ImYoutube } from "react-icons/im";
 import { FaInstagram, FaSchool } from "react-icons/fa";
-import { BsTelegram } from "react-icons/bs"
+import { BsTelegram } from "react-icons/bs";
+import { IoIosHome } from "react-icons/io";
+import { MdEmail } from "react-icons/md";
+import { FiPhoneCall } from "react-icons/fi"
 
 function Footer() {
     return (
@@ -36,9 +39,10 @@ function Footer() {
                         <p>Our school is located in Yangikurgan district of Namangan region. You can see our school on the next pages.</p>
                     </div>
                 </div>
+
                 <div className="footercenter">
                     <div className="centertitle">
-                        <h3>Bizning sahifalarimmiz:</h3>
+                        <h3 style={{ marginLeft: "-20px" }}>Bizning sahifalarimmiz:</h3>
                     </div>
                     <div className="centerlink">
                         <ul>
@@ -56,6 +60,21 @@ function Footer() {
                             </a>
                         </ul>
                     </div>
+                </div>
+
+                <div className="footerrightl">
+                    <h3>Bizning manzilimmiz:</h3>
+                    <div className="location">
+                        <ul>
+                            <li><IoIosHome /> Yangiqo'rg'on tumani Toskent ko'cha 4-uy.</li>
+                            <li><MdEmail /> 1-Maktab@gmail.com</li>
+                            <li><FiPhoneCall /> +998 99 999 9999</li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div className="footerright">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1501.1671242049713!2d71.72144298858919!3d41.19268588603301!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38a4b1bd71e9bdd7%3A0xaa0f5121af27b00c!2z0KjQutC-0LvQsCDihJYx!5e0!3m2!1suz!2s!4v1688557952091!5m2!1suz!2s" width="300" height="200" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                 </div>
             </div>
         </div>
