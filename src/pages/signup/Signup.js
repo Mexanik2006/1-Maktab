7b9d233cec94560cb73a2307b438fb1acb17579a
mexanik2006 <mexhanik2006@gmail.com>
2023-07-07 15:00:40 +0500

    ssdsd

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,13 +1,33 @@
 import React from 'react'
 import "./Signup.css"
 import { Link } from 'react-router-dom'
+import axios from 'axios'
 
 function Signup() {
+    const sendlog = async (e) => {
+        e.preventDefault()
+        let select = e.target[0].value;
+        let login = e.target[1].value;
+        let password = e.target[2].value;
+        let passwordl = e.target[3].value;
+        // console.log(login)
+        // console.log(password)
+        // console.log(passwordl)
+        // console.log(select)
+
+        await axios.post("http://localhost:1000/user/create", {
+            select,
+            login,
+            password,
+            passwordl
+        }).then(res => { console.log("ishladi") })
+            .catch(() => console.log("error chiqdi"))
+    }
     return (
         <div className='Signup'>
             <div className="signreg">
                 <h2>Registratsiya</h2>
-                <form action="">
+                <form action="" onSubmit={sendlog}>
                     <select name="" id="" required>
                         <option value="" selected>Darajangizni belgilang</option>
                         <option value="">O'qituvchi</option>
