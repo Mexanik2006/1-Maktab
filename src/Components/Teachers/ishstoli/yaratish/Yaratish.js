410df7151c9ce63c441cc8dbe5328d1b3bc71edd
richboy2008 <alphabrave777@gmail.com>
2023-07-24 16:55:06 +0500

    abdulaziz

---
+++ src/Components/Teachers/ishstoli/yaratish/Yaratish.js
@@ -0,0 +1,96 @@
+import React from 'react'
+import "./Yaratish.css"
+
+function Yaratish() {
+  return (
+    <div>
+      <div className="yaratish">
+        <div className="h1_uchun">
+          <h1><a href="/yaratish">Yangi sinfni yaratish</a></h1>
+        </div>
+        <div className="yaratish_tartib">
+          <div className="h3_1">
+            <h3>Sinf kodi</h3>
+            <div className="kodi">
+              <input type="text" />
+              <p>Misol uchun 1-a yoki 10-b</p>
+            </div>
+
+          </div>
+
+          <div className="h3_1">
+            <h3>Nomi</h3>
+            <div className="kodi">
+              <input type="text" />
+              <p>Kerak bo`lsa to`ldiriladi</p>
+            </div>
+          </div>
+
+          <div className="h3_1">
+            <h3>Smena</h3>
+            <div className="kodi">
+              <select name="" id="">
+                <option value="">-tanlang-</option>
+              </select>
+            </div>
+          </div>
+
+          <div className="h3_1">
+            <h3>Maksimal toldirish</h3>
+            <div className="kodi">
+              <input type="" />
+              <p>Joriy SAnPIN talablariga muvofiq</p>
+            </div>
+          </div>
+
+          <div className="h3_1">
+            <h3>Sinf turi</h3>
+            <div className="kodi">
+              <select name="" id="">
+                <option value="">-Sinf turini tanlang-</option>
+              </select>
+            </div>
+          </div>
+
+          <div className="h3_1">
+            <h3>Parallel</h3>
+            <div className="kodi">
+              <select name="" id="">
+                <option value=""></option>
+              </select>
+              <input type="checkbox" name="" id="" />
+            </div>
+          </div>
+
+          <div className="h3_1">
+            <h3>O`quv yili</h3>
+            <div className="kodi">
+              <input type="date" name="" id="" />
+            </div>
+          </div>
+
+          <div className="h3_1">
+            <h3>Hisobot</h3>
+            <div className="kodi">
+              <select name="" id="">
+                <option value=""></option>
+              </select>
+            </div>
+          </div>
+
+          <div className="h3_1">
+            <h3 >Qong`iroqlar j.</h3>
+            <div className="kodi">
+              <select name="" id="">
+                <option value=""></option>
+              </select>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Yaratish
--- src/Routers.js
+++ src/Routers.js
@@ -7,6 +7,8 @@ import Teachers from './Components/Teachers/Teachers';
 import Ish from './Components/Teachers/ishstoli/Ish';
 import Boshqaruv from './Components/Teachers/ishstoli/boshqaruv/Boshqaruv';
 import People from './Components/Teachers/ishstoli/people/People';
+import Sinflar from './Components/Teachers/ishstoli/sinf/Sinflar';
+import Yaratish from './Components/Teachers/ishstoli/yaratish/Yaratish';
 
 function Routers() {
     return (
@@ -19,6 +21,8 @@ function Routers() {
                 <Route path='/ish' element={<Ish />} />
                 <Route path='/boshqaruv' element={<Boshqaruv />} />
                 <Route path='/people' element={<People />} />
+                <Route path='/sinflar' element={<Sinflar />} />
+                <Route path='/yaratish' element={<Yaratish />} />
             </Routes>
         </div>
     )
