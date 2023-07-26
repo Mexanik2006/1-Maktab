19f52fa3ade3c7cf0b97c6c4f6da5f7e7d277806
mexanik2006 <mexhanik2006@gmail.com>
2023-07-26 15:57:56 +0500

    finished

--- src/Components/Teachers/myschool/teacher/Odamlar.js
+++ src/Components/Teachers/myschool/teacher/Odamlar.js
@@ -1,8 +1,27 @@
-import React from 'react'
+import React from 'react';
+import "./Odamlar.css"
+import { Link } from 'react-router-dom';
 
 function Odamlar() {
     return (
-        <div>Odamlar</div>
+        <div>
+            <div className="odamlar">
+                <div className="odamlarbgcolor">
+                    <h3>Hodimlar</h3>
+                    <div className="ogamlaruserflex">
+                        <div className="odmalarimage"></div>
+                        <div className="nameodamlar">
+                            <Link>
+                                <span>Axmadullayev</span>
+                                <span>Sadriddin</span>
+                                <span>Botirjon o'g'li</span>
+                            </Link>
+                            <span>O'qituvchi</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
